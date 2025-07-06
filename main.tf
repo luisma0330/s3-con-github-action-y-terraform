a3f35a6e72ce128769e3f7fd4b083b6f99a5b3fc
Luis Ojeda <luisma0330@hotmail.com>
2025-07-06 00:27:29 -0600

    Fix Terraform formatting

--- main.tf
+++ main.tf
@@ -4,20 +4,20 @@ provider "aws" {
 
 resource "aws_s3_bucket" "devops_bucket" {
   bucket = var.bucket_name
-   
+
   tags = {
     Name        = var.bucket_name
     Environment = "dev"
-    }
-
   }
-  
+
+}
+
 resource "aws_s3_bucket_versioning" "devops_bucket_versioning" {
   bucket = aws_s3_bucket.devops_bucket.id
-  
-    versioning_configuration {
-      status = "Enabled"
-    }
+
+  versioning_configuration {
+    status = "Enabled"
+  }
 
 
 }
